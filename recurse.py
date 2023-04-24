80729dbe24bddf0ab14f5c52df20b16a0e2e471a
SikaBoateng <109738356+SikaBoateng@users.noreply.github.com>
2023-04-24 22:28:10 +0000

    Add files via upload

---
+++ recurse.py
@@ -0,0 +1,16 @@
+def initialise(phrase):
+    words=phrase.split()
+    result=""
+    for word in words:
+        result=result+word[0].upper()
+    return result
+print(initialise("Universal Serial Bus"))
+
+def sum_numbers(numbers):
+    if numbers<=1:
+        return 1
+    # for num in range(1,numbers+1):
+        # sum=sum+num
+        # num+=1
+    return numbers+ sum_numbers(numbers-1)
+print(sum_numbers(6))
